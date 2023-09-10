c6047a2015223ca4d98020cd7d22838b3025f989
jiyamittal25 <102350835+jiyamittal25@users.noreply.github.com>
2023-09-10 15:04:33 +0530

    Update mainFile.c

--- mainFile.c
+++ mainFile.c
@@ -1,7 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<ctype.h>
 #include "Sorting.h"
+
+//function return 1 if input is integer else return 0.
+int isInteger(char *input){
+    // for negative condition
+    if(*input=='-'){
+        input++;
+    }
+
+    // for empty case
+    if(*input=='\0'){
+        return 0;
+    }
+
+    // checking each character in the input string
+    while(*input!='\0'){
+        if(!isdigit(*input)){
+            return 0;
+        }
+        input++;
+    }
+    return 1;
+}
+
 int main(){
+    // choice for datatype.
     int datatype_choice;
     printf("Enter 1 for Array of  Integer Datatype.\nEnter 2 for Array of  Character  Datatype.\nEnter 3 for Array of  Long Datatype.\nEnter 4 for Array of  Float Datatype. \nEnter 5 for Array of  Double Datatype.\n");
     scanf("%d",&datatype_choice);
@@ -10,6 +35,8 @@ int main(){
         printf("Invalid Choice for  Datatype!\n");
         exit(0);
     }
+
+    // choice for sorting algoritm.
     int sort_choice;
     printf("Enter 1 to use Selection Sort.\nEnter 2 to use Bubble Sort.\nEnter 3 to use Insertion Sort.\nEnter 4 to use Merge Sort.\nEnter 5 to use Quick Sort.\n");
     scanf("%d",&sort_choice);
@@ -18,10 +45,28 @@ int main(){
         printf("Invalid Choice for Sorting Algorithm!\n");
         exit(0);
     }
-    int size;
-    printf("Enter the size of Array : ");
-    scanf("%d",&size);
+
+   // Input of size.
+    printf("Enter the size of array: ");
+    char size_array[100];
+    scanf(" %s",&size_array);
+    int size=0;
+    if(isInteger(size_array)==0){
+        printf("Invalid Size\n");
+        exit(1);
+    }
+
+    //converting the size_array into size
+    int i=0;
+    while(size_array[i]!='\0'){
+        size=(size*10)+(size_array[i]-'0');
+        i++;
+    }
+
+    // Enter the elememts according to their datatype choice. 
     printf("Enter the Elements of Array\n");
+  
+    //if user choice integer datatype.
     if(datatype_choice==1){
         int *arr_of_int = (int *)malloc(size*sizeof(int));
         for(int i=0;i<size;i++){
@@ -51,6 +96,8 @@ int main(){
         printf("\n");
         free(arr_of_int);
     }
+
+    //if user choice character datatype.
     else if(datatype_choice==2){
         char* arr_of_char=(char *)malloc(size*sizeof(char));
         for(int i=0;i<size;i++){
@@ -80,6 +127,8 @@ int main(){
         printf("\n");
         free(arr_of_char);
     }
+
+    //if user choice long datatype.
     else if(datatype_choice==3){
         long* arr_of_long=(long *)malloc(size*sizeof(long));
         for(int i=0;i<size;i++){
@@ -109,6 +158,8 @@ int main(){
         printf("\n");
         free(arr_of_long);
     }
+
+    //if user choice float datatype.
     else if(datatype_choice==4){
         float* arr_of_float=(float *)malloc(size*sizeof(float));
         for(int i=0;i<size;i++){
@@ -138,6 +189,8 @@ int main(){
         printf("\n");
         free(arr_of_float);
     }
+
+    //if user choice double datatype.
     else if(datatype_choice==5){
         double* arr_of_double=(double *)malloc(size*sizeof(double));
         for(int i=0;i<size;i++){
