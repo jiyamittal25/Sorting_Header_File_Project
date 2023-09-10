533b118714278e20d2f6164f7b5eee61cf6630ba
jiyamittal25 <102350835+jiyamittal25@users.noreply.github.com>
2023-09-10 15:12:58 +0530

    Update InsertionSort.c

--- InsertionSort.c
+++ InsertionSort.c
@@ -3,6 +3,8 @@ void insertionSort(void* arr,int arr_size,int choice){
     for(int i=1;i<arr_size;i++){
         int a=i;
         for(int j=i-1;j>=0;j--){
+
+            // if choice is integer
             if(choice==1){
                 int elem1=((int *)arr)[a];
                 int elem2=((int *)arr)[j];
@@ -14,6 +16,8 @@ void insertionSort(void* arr,int arr_size,int choice){
                     break;
                 }
             }
+
+            // if choice is character
             else if(choice==2){
                 char elem1=((char *)arr)[a];
                 char elem2=((char *)arr)[j];
@@ -25,6 +29,8 @@ void insertionSort(void* arr,int arr_size,int choice){
                     break;
                 }
             }
+
+            // if choice is long
             else if(choice==3){
                 long elem1=((long *)arr)[a];
                 long elem2=((long *)arr)[j];
@@ -36,6 +42,8 @@ void insertionSort(void* arr,int arr_size,int choice){
                     break;
                 }
             }
+
+            //// if choice is float
             else if(choice==4){
                 float elem1=((float *)arr)[a];
                 float elem2=((float *)arr)[j];
@@ -47,6 +55,8 @@ void insertionSort(void* arr,int arr_size,int choice){
                     break;
                 }
             }
+
+            // if choice is double
             else if(choice==5){
                  double elem1=((double *)arr)[a];
                  double elem2=((double *)arr)[j];
